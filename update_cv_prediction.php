b64852b6ad96ed9de6417aa68cc484f1193d14ad
NunzioDA <nunziodamore99@gmail.com>
2025-05-04 01:03:37 +0200

    Removing unecessary echo

--- update_cv_prediction.php
+++ update_cv_prediction.php
@@ -5,7 +5,5 @@
     $info = _GET_("info");
 	$decoded_json = json_decode($info);
     file_put_contents(cv_parks, json_encode($decoded_json , JSON_PRETTY_PRINT));
-    echo "Cv prediction update ".$info;
-    echo ambient_status;
     echo json_encode($decoded_json , JSON_PRETTY_PRINT);
 ?>
